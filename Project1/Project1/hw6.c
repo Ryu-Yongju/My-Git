8889a9a6e9c52388688d3db7decde920d25f8fb1
Ryu-Yongju <ryuyongju0612@naver.com>
2021-11-16 09:56:28 +0900

    Add C File [hw6.c]

---
+++ Project1/Project1/hw6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr1[6] = { 1, 2, 3, 4, 5, 6 };
+	int arr2[6] = { 7, 8, 9, 10, 11, 12 };
+	int* ptr1 = &arr1[0];
+	int* ptr2 = &arr2[0];
+	int temp, i;
+	printf("arr1: ");
+	for (i = 0; i < 6; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\narr2: ");
+	for (i = 0; i < 6; i++)
+	{
+		printf("%d ", arr2[i]);
+	}
+	printf("\n\n");
+	for (i = 0; i < 6; i++)
+	{
+		temp = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = temp;
+		ptr1 += 1;
+		ptr2 += 1;
+	}
+	printf("after swap\narr1: ");
+	for (i = 0; i < 6; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\narr2: ");
+	for (i = 0; i < 6; i++)
+	{
+		printf("%d ", arr2[i]);
+	}
+	return 0;
+}
